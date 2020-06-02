6c8406b85ad17b8ddccf4254f0ba1e872afe9178
gccpacman <gccpacman@gmail.com>
2020-06-02 12:00:51 +0800

    101 done. https://codelabs.flutter-io.cn/codelabs/mdc-101-flutter-cn/index.htm

---
+++ mdc_100_series/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/r341h/opt/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/r341h/repos/Github/material-components-flutter-codelabs/mdc_100_series"
+export "FLUTTER_TARGET=/Users/r341h/repos/Github/material-components-flutter-codelabs/mdc_100_series/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/r341h/opt/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
